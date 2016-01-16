748799bcf19ad937f636e2c348fe11bd73b1a4fe
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-01-16 11:17:57 -0500

    added js

---
+++ index1.js
@@ -0,0 +1,13 @@
+var newArr = process.argv.slice[2];
+var sum=0;
+var sumArray=[];
+
+for (var i=0;1<newArr.length;i++) {
+  //su,+=parseFloat(newArr[i]);
+  sum=0;
+  for(var j=0); j<newArr.length;j++){
+  if(i!==j){
+    sum=parseFloat(newArr[j]);
+  }
+}
+console.log(sumArray);
